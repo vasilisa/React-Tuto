897b5ca7ac59ee2b000d5271da7fa22b6a99a1b0
vasilisa <vasilisaskv@gmail.com>
2020-01-16 17:39:39 +0000

    node version adjusted

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // Note: If you want to use something, you need to import it first. To make the component usable in other parts of the app, you need to export it after creation and import it in the file where you want to use it.
-import App from './App_DevComp'; // Note: If you want to use something, you need to import it first. To make the component usable in other parts of the app, you need to export it after creation and import it in the file where you want to use it.
+// import App from './App_DevComp'; // Note: If you want to use something, you need to import it first. To make the component usable in other parts of the app, you need to export it after creation and import it in the file where you want to use it.
 
-// import AppPart2 from './App_DevComp_part2'; // Note: If you want to use something, you need to import it first. To make the component usable in other parts of the app, you need to export it after creation and import it in the file where you want to use it.
+import AppPart2 from './App_DevComp_part2'; // Note: If you want to use something, you need to import it first. To make the component usable in other parts of the app, you need to export it after creation and import it in the file where you want to use it.
 
 // import * as serviceWorker from './serviceWorker'; // forget about it right now 
 
-ReactDOM.render(<App />, document.getElementById('root')); // We render our first page 
+ReactDOM.render(<AppPart2 />, document.getElementById('root')); // We render our first page 
 
 
 
