054ac0afbe32681e35dba4c27d17d737b45b60e8
vasilisa <vasilisaskv@gmail.com>
2020-02-05 13:57:52 +0000

    initial production

---
+++ build/precache-manifest.e33b4dc86d05915b72fe683bc7c8bf9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ceb7fb60b029f135d812ba2b0b1057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e897301456ef4e725c",
+    "url": "/static/css/main.0708e08a.chunk.css"
+  },
+  {
+    "revision": "af7f0343d055eb3341d8",
+    "url": "/static/js/2.d63f2b01.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.d63f2b01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5e897301456ef4e725c",
+    "url": "/static/js/main.536b4df3.chunk.js"
+  },
+  {
+    "revision": "dfcfac865b7110a29788",
+    "url": "/static/js/runtime-main.89a994c7.js"
+  },
+  {
+    "revision": "3da78c1f5c635fa425ae944a50b9ac86",
+    "url": "/static/media/labpic.3da78c1f.jpg"
+  },
+  {
+    "revision": "0c066e9b91a9807e2883772a9d13d93b",
+    "url": "/static/media/logo-DevComp.0c066e9b.png"
+  }
+]);
